bfd3568f28162b7ae2e5c077e47445ca33f4aeb2
THeikkila91 <tan_heikkila@hotmail.com>
2026-05-06 13:32:25 +0300

    Levelup pop up

--- r-12-gamejam/scripts/BackroundChange.gd
+++ r-12-gamejam/scripts/BackroundChange.gd
@@ -1,4 +1,5 @@
 extends VideoStreamPlayer
+@onready var level_announcer = get_node("/root/Game/ScoreUI").find_child("level_announcer", true, false)
 var active_pulse: Tween
 var main_vid = preload("res://assets/video2.ogv")
 @onready var lines_node = $"../ActivePiece"
@@ -16,13 +17,16 @@ func _process(_delta):
 		# Eri levelien muutokset taustaan
 		if lines >= 20 and current_level < 5:
 			apply_level_effects(5, 4, 0.8, Color(1, 0, 0))
+			show_level_announcement(5)
 		elif lines >= 15 and current_level < 4:
 			apply_level_effects(4, 3, 0.6, Color(1, 0.5, 0))
+			show_level_announcement(4)
 		elif lines >= 10 and current_level < 3:
 			apply_level_effects(3, 2, 0.4, Color(0.6, 0, 1))
-			
+			show_level_announcement(3)
 		elif lines >= 5 and current_level < 2:
 			apply_level_effects(2, 1, 0.2, Color(0, 0.8, 1))
+			show_level_announcement(2)
 
 func apply_level_effects(new_level: int, speed: float, darkness: float, tint: Color):
 	current_level = new_level
@@ -44,3 +48,25 @@ func apply_level_effects(new_level: int, speed: float, darkness: float, tint: Co
 	active_pulse = create_tween().set_loops()
 	active_pulse.tween_property(self, "modulate", tint * 1.5, pulse_time)
 	active_pulse.tween_property(self, "modulate", tint, pulse_time)
+
+func show_level_announcement(new_level: int):
+	if level_announcer:
+		level_announcer.text = "LEVEL " + str(new_level)
+		level_announcer.visible = true
+		level_announcer.modulate.a = 0.0
+		level_announcer.scale = Vector2(0.5, 0.5)
+		level_announcer.pivot_offset = level_announcer.size / 2
+		
+		var lt = create_tween()
+		# Appear and "Pop"
+		lt.set_parallel(true)
+		lt.tween_property(level_announcer, "modulate:a", 1.0, 0.4)
+		lt.tween_property(level_announcer, "scale", Vector2(1.3, 1.3), 0.4).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+		
+		# Wait
+		lt.set_parallel(false)
+		lt.tween_interval(1.5)
+		
+		# Disappear
+		lt.tween_property(level_announcer, "modulate:a", 0.0, 0.5)
+		lt.tween_callback(func(): level_announcer.visible = false)
