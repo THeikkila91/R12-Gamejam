072b01bd16a2e274ef11bffd584018128ae755d5
THeikkila91 <tan_heikkila@hotmail.com>
2026-05-07 10:40:13 +0300

    Aloitus ruudun äänet

--- r-12-gamejam/scripts/StartScreenScript.gd
+++ r-12-gamejam/scripts/StartScreenScript.gd
@@ -1,7 +1,17 @@
 extends VBoxContainer
 
 func _on_start_game_pressed():
+	get_node("../buttonpresssound").play()
+	await get_tree().create_timer(0.4).timeout
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_quit_pressed():
+	get_node("../buttonpresssound").play()
+	await get_tree().create_timer(0.4).timeout
 	get_tree().quit()
+	
+func _on_start_game_mouse_entered():
+	get_node("../hover").play()
+
+func _on_quit_mouse_entered():
+	get_node("../hover").play()
